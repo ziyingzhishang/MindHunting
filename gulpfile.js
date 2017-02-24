8f3f5493abe29649b9a05604df2829725db4fe7f
xudong <xudong@xmind.net>
2017-02-24 15:15:26 +0800

    Add gulp config for mixmind .

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+const gp = require('gulp');
+
+/** build the project for developers. */
+gp.task("build", function() {
+
+});
+
+/** release the project for users . */
+gp.task("release", function() {
+
+});
